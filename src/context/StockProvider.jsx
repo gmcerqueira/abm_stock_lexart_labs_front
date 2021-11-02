c36907b34ed42a0b999fc6b54dd902d5177a8899
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-11-02 15:30:34 -0300

    fetch Products when component renders

--- src/context/StockProvider.jsx
+++ src/context/StockProvider.jsx
@@ -3,14 +3,31 @@ import PropTypes from 'prop-types';
 
 const StockContext = createContext();
 
+const CRUD_URL = 'https://crudcrud.com/api/577239c7a0d1445a87232d17665c6e5c/products';
+
 const StockProvider = ({ children }) => {
   const [Products, setProducts] = useState([]);
+  const [Error, setError] = useState(false);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch(CRUD_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json());
+
+      setProducts(response);
+    } catch (error) {
+      setError(error);
+    }
+  };
 
   useEffect(() => {
-    setProducts(['aaaaa', 'bbbbb', 'ccccc']);
+    getProducts();
   }, []);
-
-  const context = { Products };
+  const context = { Products, Error };
 
   return (
     <StockContext.Provider value={context}>{children}</StockContext.Provider>
