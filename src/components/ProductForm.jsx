744be103e55303e10b64f2faf9bcc4aacdcc9f11
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-11-05 18:39:43 -0300

    style form with bootstrap

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { StockContext } from '../context/StockProvider';
 import FormInput from './FormInput';
 
@@ -12,71 +12,76 @@ const ProductForm = () => {
     StockItem,
     resetItemInicialState,
   } = useContext(StockContext);
+
+  const toEdit = Object.prototype.hasOwnProperty.call(StockItem, '_id');
+
   return (
-    <Form>
-      <div>
-        Product:
-        <FormInput
-          label="Name"
-          name="name"
-          value={StockItem.product.name}
-          onChange={handleProductChange}
-        />
-        <FormInput
-          label="Valid until"
-          name="validUntil"
-          type="date"
-          value={StockItem.product.validUntil}
-          onChange={handleProductChange}
-        />
-        <FormInput
-          label="Quantity"
-          name="quantity"
-          type="number"
-          value={StockItem.quantity ? StockItem.quantity : ''}
-          onChange={handleStockItemChange}
-        />
-        <FormInput
-          label="Price"
-          name="price"
-          type="number"
-          value={StockItem.price ? StockItem.price : ''}
-          onChange={handleStockItemChange}
-        />
-        <Form.Check
-          type="switch"
-          name="active"
-          label="Active?"
-          feedbackTooltip
-          checked={StockItem.active}
-          onChange={handleStockItemChange}
-        />
-      </div>
-      <div>
-        Client:
-        <FormInput
-          label="Name"
-          name="name"
-          value={StockItem.client.name}
-          onChange={handleClientChange}
-        />
-        <FormInput
-          label="Email"
-          name="email"
-          type="email"
-          value={StockItem.client.email}
-          onChange={handleClientChange}
-        />
-      </div>
-      <button type="button" onClick={shouldSubmitOrEdit}>
-        {Object.prototype.hasOwnProperty.call(StockItem, '_id')
-          ? 'Edit'
-          : 'Create'}
-      </button>
-      <button type="button" onClick={resetItemInicialState}>
+    <section className="">
+      <h1>New Product</h1>
+      <Form className="d-flex ">
+        <div className="me-3">
+          <h3>Product</h3>
+          <FormInput
+            label="Name"
+            name="name"
+            value={StockItem.product.name}
+            onChange={handleProductChange}
+          />
+          <FormInput
+            label="Valid until"
+            name="validUntil"
+            type="date"
+            value={StockItem.product.validUntil}
+            onChange={handleProductChange}
+          />
+          <FormInput
+            label="Quantity"
+            name="quantity"
+            type="number"
+            value={StockItem.quantity ? StockItem.quantity : ''}
+            onChange={handleStockItemChange}
+          />
+          <FormInput
+            label="Price"
+            name="price"
+            type="number"
+            value={StockItem.price ? StockItem.price : ''}
+            onChange={handleStockItemChange}
+          />
+          <Form.Check
+            type="switch"
+            name="active"
+            label="Active?"
+            feedbackTooltip
+            checked={StockItem.active}
+            onChange={handleStockItemChange}
+            className="mb-4"
+          />
+        </div>
+        <div>
+          <h3>Client</h3>
+          <FormInput
+            label="Name"
+            name="name"
+            value={StockItem.client.name}
+            onChange={handleClientChange}
+          />
+          <FormInput
+            label="Email"
+            name="email"
+            type="email"
+            value={StockItem.client.email}
+            onChange={handleClientChange}
+          />
+        </div>
+      </Form>
+      <Button onClick={shouldSubmitOrEdit} className="me-3">
+        {toEdit ? 'Update' : 'Submit'}
+      </Button>
+      <Button variant="secondary" onClick={resetItemInicialState}>
         Clear
-      </button>
-    </Form>
+      </Button>
+    </section>
   );
 };
 
